5387b43373ebcef24db6e5aa1a879025ba2c8365
cindy <cindy.choi@nexr.com>
2020-11-05 19:27:14 +0900

    [HEADER] Create a header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <Game />
+  <Header />
 </template>
 
 
 <script>
 import Game from '@/components/Game'
+import Header from '@/components/Header'
 export default {
   name: 'App',
-  components: { Game }
+  components: {
+    Game,
+    Header,
+  }
 }
 </script>
 
 
 <style lang="scss">
-body {
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0;
-}
 </style>
