77f61462538b70808dc4c819006891e5746296b9
cindy <cindy.choi@nexr.com>
2020-11-05 18:35:06 +0900

    [BLUSH-001] 헤더에 마우스 오버 효과 넣기

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,18 +5,20 @@
       :key="menu.title"
       class="header-menu"
       :style="{
-        '--menu-depth': (index+1)*0.2,
-        '--pointer-top': top,
-        '--pointer-left': left,
+        '--menu-depth': index,
       }"
-      @mouseover="handleMouseOver"
-      @mouseout="handleMouseOut"
     >
+      <span>
+        {{ menu.title }}
+      </span>
       <span
         v-for="i in 3"
         :key="i"
+        class="moving-object"
+        :style="{
+          '--object-number': i + 1,
+        }"
       />
-      {{ menu.title }}
     </div>
   </div>
 </template>
@@ -35,15 +37,6 @@ export default {
     
   }),
   methods: {
-    handleMouseOver(event) {
-      console.log('over', event);
-      this.top = event.pageY;
-      this.left = event.pageX;
-    },
-
-    handleMouseOut(event) {
-      console.log('out', event);
-    }
   }
 }
 </script>
@@ -55,36 +48,66 @@ export default {
 }
 
 .header-menu {
-  height: 100%;
+  width: 7rem;
+  height: 3rem;
   display: flex;
   align-items: center;
-  padding: 1rem;
   color: #000;
+  overflow: hidden;
 
   span {
     position: absolute;
-    display: block;
-    content: "";
-    z-index: 0;
-    width: 0;
-    height: 0;
-    top: var(--pointer-top);
-    left: var(--pointer-left);
-
-    border-radius: 100%;
-    background: rgba(218, 57, 57, var(--menu-depth));
-
-    transition: all ease 0.7s;
+    z-index: 1;
   }
 
+  .moving-object {
+    position: absolute;
+    display: block;
+    width: 3rem;
+    height: 3rem;
+    z-index: 0;
+    border-radius: 100%;
+    background: transparent;
+    animation: rotate calc(var(--object-number) * 1.3s) linear infinite;
+  }
 
   &:hover {
     color: #fff;
+    transition: color 0.2s linear;
+    cursor: pointer;
 
-    span {
-      width: 2rem;
-      height: 2rem;
+    .moving-object {
+      transition: background-color 1000ms linear;
+      background: rgba(218, 57, 57, calc((var(--menu-depth) + 1) * 0.1));
     }
+  }
+}
+
+@keyframes fadein {
+  from { opacity: 0 }
+  to { opacity: 1 }
+}
+
+@keyframes rotate {
+  0% {
+    top: calc(0.6rem);
+    left: calc((0.7rem + var(--menu-depth) * 16rem) + 2rem);
+  }
+  25% {
+    top: calc(0.6rem + 0.6rem);
+    left: calc(0.7rem + var(--menu-depth) * 16rem);
+  }
+  50% {
+    top: calc(0.6rem + 1.2rem);
+    left: calc((0.7rem + var(--menu-depth) * 16rem) + 2rem);
+  }
+  75% {
+    top: calc(0.6rem + 0.6rem);
+    left: calc((0.7rem + var(--menu-depth) * 16rem) + 4rem);
+  }
+  100% {
+    top: 0.6rem;
+    left: calc((0.7rem + var(--menu-depth) * 16rem) + 2rem);
   }
 }
 </style>
