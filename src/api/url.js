2792318df654ae4607d30bd75fe20a4573184095
cindy <cindy.choi@nexr.com>
2021-01-14 19:25:22 +0900

    Fix wrong url.

--- src/api/url.js
+++ src/api/url.js
@@ -1,6 +1,6 @@
 // default values
 const DEV_A3S_BASE = process.env.VUE_APP_A3S_BASE_DEV;
-const QA_A3S_BASE = process.env.VUE_APP_QA_A3S_BASE_QA;
+const QA_A3S_BASE = process.env.VUE_APP_A3S_BASE_QA;
 const DEV_A3S_PORT = process.env.VUE_APP_A3S_PORT_DEV;
 const QA_A3S_PORT = process.env.VUE_APP_A3S_PORT_QA;
 
