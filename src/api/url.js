4ea4804f25ca58a38f28241e444e7b608f8fabb0
cindy <cindy.choi@nexr.com>
2021-05-03 10:33:01 +0900

    NEA url update

--- src/api/url.js
+++ src/api/url.js
@@ -1,12 +1,10 @@
 // default values
 const DEV_A3S_BASE = process.env.VUE_APP_A3S_BASE_DEV;
 const QA_A3S_BASE = process.env.VUE_APP_A3S_BASE_QA;
-const DEV_A3S_PORT = process.env.VUE_APP_A3S_PORT_DEV;
-const QA_A3S_PORT = process.env.VUE_APP_A3S_PORT_QA;
 
 export default {
   A3S: {
-    DEV: `${DEV_A3S_BASE}:${DEV_A3S_PORT}`,
-    QA: `${QA_A3S_BASE}:${QA_A3S_PORT}`,
+    DEV: `${DEV_A3S_BASE}`,
+    QA: `${QA_A3S_BASE}`,
   },
 };
